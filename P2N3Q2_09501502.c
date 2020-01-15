26b5adb6805f9f98dbc6e25351c793ca9273da0e
smpny7 <symphonymode7@icloud.com>
2020-01-15 17:37:00 +0900

    次構造体にしよう

--- P2N3Q2_09501502.c
+++ P2N3Q2_09501502.c
@@ -1,28 +1,50 @@
 #include <stdio.h>
 
-int main(void) {
-    FILE *fin;
-    fin = fopen("score.csv", "r");
-
-    int i, j, student[80], eng[80], math[80], japa[80], sci[80];
-    char raw[256]={}, user[80][256]={};
-    for(i=0; fgets(raw, 256, fin)!=NULL; i++) {
-        // printf("%s", raw);
-        for(j=0; raw[j]!='\0'; j++) {
-            user[i][j] = raw[j];
-        }
-        // printf(">%d %s", i, user[i]);
+char rank(int score) {
+    if (score>=101) {
+        return 'S+';
+    } else if (score>=90 && score<=100) {
+        return 'S';
+    } else if (score>=80 && score<=89) {
+        return 'A';
+    } else if (score>=70 && score<=79) {
+        return 'B';
+    } else if (score>=60 && score<=69) {
+        return 'C';
+    } else if (score>=0 && score<=59) {
+        return 'F';
+    } else {
+        return 'X';
     }
-    scanf(user[0], "%d, %d, %d, %d, %d", &student[0], &eng[0], &math[0], &japa[0], &sci[0]);
-    printf("%d", math[0]);
+}
 
+int main(void) {
+    FILE *fin, *fout1, *fout2, *fout3, *fout4;
+    fin = fopen("score.csv", "r");
+    fout1 = fopen("grade_Eng.csv", "w");
+    fout2 = fopen("grade_Math.csv", "w");
+    fout3 = fopen("grade_Japa.csv", "w");
+    fout4 = fopen("grade_Sci.csv", "w");
 
+    int i, j, score, mem[5], student[80]={}, eng[80]={}, math[80]={}, japa[80]={}, sci[80]={};
+    for(i=0; fscanf(fin, "%d, %d, %d, %d, %d", &mem[0], &mem[1], &mem[2], &mem[3], &mem[4]) != EOF; i++) {
+        student[i] = mem[0];
+        eng[i] = mem[1];
+        math[i] = mem[2];
+        japa[i] = mem[3];
+        sci[i] = mem[4];
+    }
 
+    for(j=0; j<i; j++) {
+        fprintf(fout1, "%d, %c\n", student[j], rank(eng[j]));
+    }
 
-        // for(i=0; ss[i]=='\n'; i++) {
-        //     user1[i] = ss[i];
-        // }
-        // fscanf(user1, "%d, %d, %d, %d, %d", &student[0], &eng[0], &math[0], &japa[0], &sci[0]);
+    fclose(fin);
+    fclose(fout1);
+    fclose(fout2);
+    fclose(fout3);
+    fclose(fout4);
+ 
+    return 0;
 
-    // printf("%d", student[0]);
 }
