6a8698da4e90d2476de45abf2801f419f31079c0
smpny7 <symphonymode7@icloud.com>
2020-01-15 11:31:00 +0900

    0115_4 課題1完了

--- P2N3Q1_09501502.c
+++ P2N3Q1_09501502.c
@@ -2,6 +2,13 @@
 #include <math.h>
 #define pi 3.1415
 
+int funNum(void);
+int loopCheck(int num);
+double inPar(void);
+void fun1(int num);
+void fun2(int num);
+void fun3(int num);
+
 int funNum(void) {
     int num;
     printf("Input a function number:\n");
@@ -10,25 +17,29 @@ int funNum(void) {
     return num;
 }
 
-int integrity(int num) {
+int loopCheck(int num) {
     if(num == -1) {
         printf("The function calculator quits.\n");
         return 0;
 
     } else if (num == 1) {
         fun1(num);
-        return 0;
+        printf("\n");
+        return 1;
 
     } else if (num == 2) {
         fun2(num);
-        return 0;
+        printf("\n");
+        return 1;
 
     } else if (num == 3) {
         fun3(num);
-        return 0;
+        printf("\n");
+        return 1;
         
     } else {
         printf("Input a correct function number from 1 to 3.\n");
+        printf("\n");
         return 1;
     }
 }
@@ -43,14 +54,26 @@ double inPar(void) {
 void fun1(int num) {
     double value;
     value = inPar();
+    printf("cos(%3.2f) is %.4f.\n", value, cos(value*pi/180));
+}
+
+void fun2(int num) {
+    double value;
+    value = inPar();
+    printf("exp(%.2f) is %.4f.\n", value, exp(value));
+}
+
+void fun3(int num) {
+    double value;
+    value = inPar();
+    printf("abs(%.2f) is %.4f.\n", value, fabs(value));
 }
 
 int main(void) {
-    int err = 1;
-    while(err) {
+    int loop = 1;
+    while(loop) {
         int num = funNum();
-        err = integrity(num);
+        loop = loopCheck(num);
     }
-
     return 0;
 }
