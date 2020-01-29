c514281544d3ec9a17a9f5951e7f5f5a46c6e024
smpny7 <symphonymode7@icloud.com>
2020-01-29 10:18:26 +0900

    できた？

--- P2N4Q4_09501502.c
+++ P2N4Q4_09501502.c
@@ -10,7 +10,7 @@ double delta(char *xd) {
         x += xd[i];
     }
     x = x * x;
-    return x;
+    return 7*x2-x;
 }
 
 void cal_a(double *a, char *xd, char *yd) {
@@ -31,32 +31,35 @@ void cal_a(double *a, char *xd, char *yd) {
 
 void cal_b(double *b, char *xd, char *yd) {
     int i;
-    double x2, del;
+    double y, x2, x, xy, del;
+    for(i=0;i<6;i++) {
+        y += yd[i] * yd[i];
+    }
     for(i=0;i<6;i++) {
         x2 += xd[i];
     }
     x2 = x2 * x2;
     for(i=0;i<6;i++) {
-        xy += xd[i] * yd[i];
-    }
-    for(i=0;i<6;i++) {
         x += xd[i] * xd[i];
     }
     for(i=0;i<6;i++) {
-        y += yd[i] * yd[i];
+        xy += xd[i] * yd[i];
     }
     del = delta(xd);
-    *b = ((7*xy) - x*y) / del;
+    *b = (y*x2 - x*xy) / del;
 }
 
 
 int main(void) {
-    double a, b;
+    double *a, *b;
     double xd[6] = {1.0, 2.0, 3.0, 4.0, 5.0, 6.0};
     double yd[6] = {5.0, 14.4, 23.1, 32.3, 41.0, 50.4};
 
     cal_a(&a, xd, yd);
     cal_b(&b, xd, yd);
+
+    printf("%f\n", a);
+    printf("%f\n", b);
 
     return 0;
 }
